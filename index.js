1e31ae9bd778c6df91bd30e547f44048dcc2255d
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-25 14:46:01 +0000

    Update welcome message in Vietnamese for clarity

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Chào mừng đến với Vi NFT Marketplace!');
+    res.send('Chào mừng bạn đến với Thị Trường NFT Việt Nam!');
 });
 
 app.listen(PORT, () => {
