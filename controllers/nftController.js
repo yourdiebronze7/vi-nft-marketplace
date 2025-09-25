e724a483042b2ed6e9085ec0747d9088147024ad
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-25 14:42:58 +0000

    feat: initial commit for controllers/nftController.js

---
+++ controllers/nftController.js
@@ -0,0 +1,9 @@
+const nftModel = require('../models/nftModel');
+
+exports.getAllNFTs = (req, res) => {
+  // Logic để lấy tất cả NFT
+};
+
+exports.createNFT = (req, res) => {
+  // Logic để tạo NFT
+};
