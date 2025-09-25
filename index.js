bbb7d969c947a9c53fa10a720c225a7d5e6c51e8
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-25 14:44:06 +0000

    Fix indentation for consistency in index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng đến với Vi NFT Marketplace!');
+    res.send('Chào mừng đến với Vi NFT Marketplace!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Máy chủ đang chạy trên http://localhost:${PORT}`);
+    console.log(`Máy chủ đang chạy trên http://localhost:${PORT}`);
 });
